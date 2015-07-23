5623b8f32988093851cc7d019633b965a2febc50
Eric Krans <ekrans@albany.edu>
2015-07-23 12:43:48 -0400

    css styles

--- assets/react/components/utils/MapSidebar.react.js
+++ assets/react/components/utils/MapSidebar.react.js
@@ -78,6 +78,7 @@ var MapSidebar = React.createClass({
         var tabs = this.props.panes.map(function(d,i){
             var tabClass = 'sidebar-tabs-li ';
                 tabClass += d.name === scope.state.activePane ? 'active' : '';
+            }
            
             return (
                 <Tappable onTap={scope.setActivePane.bind(null,d.name)} id={d.name+'_tap'}>
