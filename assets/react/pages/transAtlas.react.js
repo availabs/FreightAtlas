31aabf4e7dd34a928946d0257200ab88f3a1bba0
Myan <r.k.dubowsky@gmail.com>
2015-08-18 14:56:00 -0400

    Created two files that hold the list of attributes for Transearch DBs. Transearch page now lets you choose from 2004 or 2012 dataset and lets you choose if you want a where clause

--- assets/react/pages/transAtlas.react.js
+++ assets/react/pages/transAtlas.react.js
@@ -1,41 +1,90 @@
 'use strict';
 
-var React = require('react');
+var React = require('react'),
+
+    AttList2004 = require('../data/attList2004'),
+    AttList2012 = require('../data/attList2012');
+
+
+
+
+var AttributeButton = React.createClass({
+    getInitialState:function(){
+        return({value:""})
+    },
+    handleChange:function(event){
+        this.setState({value:event.target.value})
+    },
+    componentWillRecieveProps:function(newProps){
+        this.setState(newProps.value)
+    },
+    render:function(){
+            if(this.props.version == "select"){
+                return(
+                        <div>
+                            <input type="radio" name="column" value={this.props.value} onChange={this.handleChange}/>
+                            {this.props.value}
+                        </div>
+                    )
+            }
+            else{
+                return(
+                        <div>
+                            <input type="checkbox" name="column" value={this.props.value} onChange={this.handleChange}/>
+                            {this.props.value}
+                        </div>
+                    )                
+            }       
+    }
+})
 
 
 // Will be a list of attributes passed as a property
 // If called by select portion, will be checkboxes
 // If called by where portion, will be radio button
 var AttributeList = React.createClass({
-
-
+    shouldComponentUpdate: function(nextProps, nextState) {
+      return this.props.db !== nextProps.db;
+    },
     render: function(){
 
 //Will eventually pass a list of attributes
+    var scope = this;
 
-            
-            //Will either be select or where
-            if(this.props.version == "select"){
+        if(!this.props.db){
+            var attList = [];
+        }
+        else if(this.props.db == "2004"){
+            var attList = AttList2004
+        }
+        else{
+            var attList = AttList2012            
+        }
 
+
+
+        var list = Object.keys(attList).map(function(key,index){
+
+            if(scope.props.version == "select"){
                 return(
-                    <div>
-                        <input type="radio" name="column" value="id"/>
-                        <br/>
-                        <input type="radio" name="column" value="name"/>
-                    </div>
+                        <AttributeButton version="select" value={key}/>
                     )
             }
             else{
+                return(
+                        <AttributeButton version="checkbox" value={key}/>
+                    )                
+            }
+        })
 
+        console.log("list of buttons",list)
                 return(
                     <div>
-                        <input type="checkbox" name="column" value="id">id</input>
-                        <br/>
-                        <input type="checkbox" name="column" value="name">name</input>
+                        {list}
                     </div>
                     )
 
-            }
+            
 
     
     }
@@ -43,18 +92,6 @@ var AttributeList = React.createClass({
 
 var DbList = React.createClass({
 
-    getInitialState:function(){
-        return {
-            db:""
-        }
-    },
-    onChange:function(e){
-        console.log("changing");
-        this.setState({
-            db: e.currentTarget.value
-        });
-
-    },
     render: function(){
         return(
             <div>
@@ -86,8 +123,12 @@ var TransDashboard = React.createClass({
                 db:name
             })
       },
+    onWhereClick: function(event){
+        this.getDOMNode.checked = !this.getDOMNode.checked
+    },
     render: function() {
-        console.log(this.state);
+
+        var scope = this;
 //Want a checkbox button list of attributes that can be selected.
 //Want Where clause which, when checked opens up radio buttons for attributes, a radio for <,>,=
 //Some way to finish the where clause
@@ -97,10 +138,33 @@ var TransDashboard = React.createClass({
 //Want to make the data sortable by column
         return (
                 <span style={{width:'100%',height:'100%'}} >
-                    <h4>Transearch API </h4>
-                    <h5>Now Testing Git </h5>
-                    <DbList onClick={this.handleChildClick} />
-                    <AttributeList />
+                    <h2>Transearch API </h2>
+                    <table style={{borderSpacing:"30px"}}>
+                        <tr>
+                            <td>
+                                <h5> Choose a database to query </h5>
+                            </td>
+                            <td>
+                                <h5> Choose 1 attribute for SELECT portion of query </h5>
+                            </td>
+                            <td>
+                                <h5>Toggle WHERE clause</h5>
+                            </td>
+                        </tr>
+                        <tr style={{verticalAlign: "top"}}>
+                            <td style={{verticalAlign: top}}>
+                                <DbList onClick={this.handleChildClick} />
+                            </td>     
+                            <td style={{verticalAlign: "top"}}>
+                                <AttributeList db={scope.state.db} version="select"/>
+                            </td>
+                            <td style={{verticalAlign: "top"}}>
+                                <input type="checkbox" name="whereClause" defaultChecked={false} onChange={this.onWhereClick}/>
+                                <h7>WHERE</h7>
+                            </td>                 
+                        </tr>                         
+                                                                              
+                    </table>
                 </span>
         )
     
